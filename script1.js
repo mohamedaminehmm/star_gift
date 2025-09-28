07326d02555d4d43767e3f256e941151b5337374
mohamedaminehmm <151930331+mohamedaminehmm@users.noreply.github.com>
2025-09-28 09:43:36 +0100

    Update script1.js

--- script1.js
+++ script1.js
@@ -21,7 +21,7 @@ const scene = new THREE.Scene()
 
 //galaxy
 const parameters = {}
-parameters.count = 75000;
+parameters.count = 67000;
 parameters.size = 0.01;
 parameters.radius = 2.15; 
 parameters.branches = 3; 
